efcfa99c4bd6911bef0a899de241799b6f537895
Khromushkin Nikita <n.khromushkin@qiwi.com>
2020-04-11 21:12:17 +0300

    remove product

--- product/triggers.sql
+++
@@ -1,7 +0,0 @@
-create or replace trigger change_product_b_iud_strict
-    before insert or update or delete
-    on product
-begin
-    product_pack.product_tr_body_restrict();
-end change_product_b_iud_strict;
-/
